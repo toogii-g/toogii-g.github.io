540a2a9efe37c9c42d92fa16d1ecc234d06b9457
bitoogii <bitoogii@yahoo.com>
2024-06-12 14:33:52 -0500

    add maze

---
+++ w3d3/maze.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+        let fault=false;
+        let start=true;
+    $("#start").click(function(){
+        if(start===true){
+            $(".boundary").removeClass('youlose');
+            $("#status").text('Click the "S" to begin.');
+            
+        }
+            fault=false;
+            start=true;
+        $(".boundary").mouseover(function(){
+            if(start===true){
+                $(".boundary").addClass('youlose');
+                fault=true;
+            }
+        });
+
+        $("#end").mouseover(function(){
+            if(start===true){
+                if(fault!==true){
+                    $("#status").text("You win");
+                } else {
+                    $("#status").text("You lose");
+                }
+            }
+            start=false;
+        });
+    
+        $("#maze").mouseleave(function(){
+            if(start===true){
+            $(".boundary").addClass('youlose');
+            $("#status").text("You lose");
+            }
+            start=false;
+        });
+    
+        $(".boundary").removeClass('youlose');
+        $("#status").text('Click the "S" to begin.');
+        
+    });
+
+});
